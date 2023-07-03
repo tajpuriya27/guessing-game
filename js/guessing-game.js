21e256f08c382d177ddb050fc6ceb4070f7e3a83
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-03 20:50:50 +0545

    guessing-game v1

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -37,7 +37,7 @@ class Game {
     this.playersGuess = null;
     this.pastGuesses = [];
     this.winningNumber = generateWinningNumber();
-    this.playerLife = 1;
+    this.playerLife = 5;
   }
 
   difference() {
@@ -48,12 +48,16 @@ class Game {
     return this.playersGuess < this.winningNumber ? true : false;
   }
 
+  // this message is shown in title-msg
   checkGuess() {
     if (this.playersGuess === this.winningNumber) {
+      --this.playerLife;
+      this.pastGuesses.push(this.playersGuess);
       return "You Win!";
     } else if (this.pastGuesses.includes(this.playersGuess)) {
       return "You have already guessed that number.";
     } else {
+      --this.playerLife;
       this.pastGuesses.push(this.playersGuess);
     }
 
@@ -70,19 +74,24 @@ class Game {
   }
 
   playersGuessSubmission(submittedNum) {
-    this.playerLife++;
-    if (this.playerLife <= 5) {
-      if (
-        submittedNum > 100 ||
-        submittedNum < 1 ||
-        typeof submittedNum !== "number"
-      ) {
-        throw "That is an invalid guess.";
-      }
+    if (this.playerLife === 1 && !this.pastGuesses.includes(submittedNum)) {
+      --this.playerLife;
+      this.playersGuess = submittedNum;
+      this.pastGuesses.push(this.playersGuess);
+      return "You Lose.";
+    }
+
+    if (
+      submittedNum > 100 ||
+      submittedNum < 1 ||
+      typeof submittedNum !== "number"
+    ) {
+      titleMsg.innerText = "Please Input number between 1-100.";
+      document.getElementById("num-input").value = "";
+      throw "That is an invalid guess..";
+    } else {
       this.playersGuess = submittedNum;
       return this.checkGuess();
-    } else {
-      return "You Lose.";
     }
   }
 
@@ -97,4 +106,64 @@ class Game {
 
 function newGame() {
   return new Game();
+}
+
+// Jasmine shows some  error at line 87. Need to check.
+
+// let game = new Game();
+// console.log(game.playersGuessSubmission(101));
+
+// DOM part
+let game = new Game();
+
+// unique selectors
+const titleMsg = document.getElementById("title-msg");
+const guessMsg = document.getElementById("guess-msg");
+const submitClick = document.getElementById("submit-btn");
+const listItem = document.getElementById("list-items").children;
+const resetClick = document.getElementById("reset-btn");
+
+// Handling Reset.
+resetClick.addEventListener("click", resetFun);
+function resetFun() {
+  for (let i = 0; i < listItem.length; ++i) {
+    listItem[i].innerText = "";
+  }
+  game = newGame();
+  document.getElementById("num-input").value = "";
+}
+
+// Handling click
+submitClick.addEventListener("click", clickFun);
+function clickFun() {
+  const guessedNum = Number(document.getElementById("num-input").value);
+  titleMsg.innerHTML = game.playersGuessSubmission(guessedNum);
+
+  document.getElementById("num-input").value = "";
+
+  for (let i = 0; i < game.pastGuesses.length; ++i) {
+    if (game.pastGuesses[i]) {
+      listItem[i].innerText = game.pastGuesses[i];
+    }
+  }
+
+  setTimeout(() => {
+    guessMsg.innerHTML =
+      guessedNum < game.winningNumber
+        ? "Guess Higher!"
+        : guessedNum === game.winningNumber
+        ? "You Win!"
+        : "Guess Lower!";
+  }, "1000");
+
+  setTimeout(() => {
+    guessMsg.innerHTML = "Guess a number between 1-100";
+  }, "2000");
+}
+
+// Hint button logic
+const hintBtn = document.getElementById("hint-btn");
+hintBtn.addEventListener("click", hintFun);
+function hintFun() {
+  titleMsg.innerHTML = `The hints for the game is [${game.provideHint()}]`;
 }
